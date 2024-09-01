e18b72474e5b90f29b18e02b036c6c4e33df45d2
Alan Coopersmith <alan.coopersmith@oracle.com>
2024-08-31 17:13:25 -0700

    Remove remnants of support for SVR4 systems other than Solaris & illumos
    Most of the support for such OS'es was removed in 2010 for
    xorg-server-1.10.0, but a few bits lingered on.
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1667>

--- hw/xfree86/os-support/xf86_OSlib.h
+++ hw/xfree86/os-support/xf86_OSlib.h
@@ -81,10 +81,9 @@
 #include <stddef.h>
 
 /**************************************************************************/
-/* SVR4, including Solaris                                                */
+/* Solaris or illumos-based system                                        */
 /**************************************************************************/
-#if (defined(SVR4)) && \
-    (defined(__sun) || defined(__i386__))
+#if defined(__SVR4) && defined(__sun)
 #include <sys/ioctl.h>
 #include <signal.h>
 #include <termio.h>
@@ -93,45 +92,10 @@
 
 #include <errno.h>
 
-#if defined(_NEED_SYSI86)
-#if !(defined (__sun) && defined (SVR4))
-#include <sys/immu.h>
-#include <sys/region.h>
-#include <sys/proc.h>
-#endif
-#include <sys/tss.h>
-#include <sys/sysi86.h>
-#if defined(SVR4) && !defined(__sun)
-#include <sys/seg.h>
-#endif                          /* SVR4 && !__sun */
-/* V86SC_IOPL was moved to <sys/sysi86.h> on Solaris 7 and later */
-#if !defined(V86SC_IOPL)        /* Solaris 7 or later? */
-#include <sys/v86.h>            /* Nope */
-#endif
-#if defined(__sun) && (defined (__i386__) || defined(__i386) || defined(__x86))  && defined (SVR4)
-#include <sys/psw.h>
-#endif
-#endif                          /* _NEED_SYSI86 */
-
-#if defined(HAS_SVR3_MMAPDRV)
-#include <sys/sysmacros.h>
-#if !defined(_NEED_SYSI86)
-#include <sys/immu.h>
-#include <sys/region.h>
-#endif
-#include <sys/mmap.h>           /* MMAP driver header */
-#endif
-
-#if !defined(__sun) || defined(HAVE_SYS_VT_H)
+#ifdef HAVE_SYS_VT_H
 #define HAS_USL_VTS
 #endif
-#if !defined(__sun)
-#include <sys/emap.h>
-#endif
-#if   defined(HAS_USL_VTS)
-#if !defined(__sun)
-#include <sys/at_ansi.h>
-#endif
+#ifdef HAS_USL_VTS
 #include <sys/kd.h>
 #include <sys/vt.h>
 
@@ -139,16 +103,9 @@ extern _X_HIDDEN void xf86VTAcquire(int);
 extern _X_HIDDEN void xf86VTRelease(int);
 #endif
 
-#if !defined(VT_ACKACQ)
-#define VT_ACKACQ 2
-#endif                          /* !VT_ACKACQ */
-
-#if !(defined(__sun) && defined (SVR4))
-#define DEV_MEM "/dev/pmem"
-#endif
 #define CLEARDTR_SUPPORT
 
-#endif                          /* SVR4 */
+#endif                          /* SVR4 && __sun */
 
 /**************************************************************************/
 /* Linux or Glibc-based system                                            */
