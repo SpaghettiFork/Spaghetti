b95a1c69c0c47738739772486d413553b653f783
Alan Coopersmith <alan.coopersmith@oracle.com>
2025-04-13 12:44:41 -0700

    os: remove unused definition of BUGADDRESS
    We stopped including this in error messages back in 2008
    
    Clears warning from clang 19.1.7:
    
    os/utils.c:1338:9: warning: macro is not used [-Wunused-macros]
     1338 | #define BUGADDRESS BUILDERADDR
          |         ^
    
    Fixes: ef77e4c44 ("Remove useless commentary from environment and argument processing.")
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1910>

--- os/utils.c
+++ os/utils.c
@@ -1779,14 +1779,6 @@ enum BadCode {
     InternalError
 };
 
-#if defined(VENDORSUPPORT)
-#define BUGADDRESS VENDORSUPPORT
-#elif defined(BUILDERADDR)
-#define BUGADDRESS BUILDERADDR
-#else
-#define BUGADDRESS "xorg@freedesktop.org"
-#endif
-
 void
 CheckUserParameters(int argc, char **argv, char **envp)
 {
