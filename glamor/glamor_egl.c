SpaghettiFork/Spaghetti

commit aa1af2513a3bc508815f6a4249728662ddf1b49d

glamor: Prefer using gbm_bo_map/gbm_bo_unmap
Prefer using gbm_bo_map/gbm_bo_unmap instead of glamor_transfer, to
avoid buffer copying.

glamor/glamor_egl.c
@@ -190,6 +190,8 @@ glamor_egl_set_pixmap_bo(PixmapPtr pixmap, struct gbm_bo *bo,
     struct glamor_pixmap_private *pixmap_priv =
         glamor_get_pixmap_private(pixmap);
 
+    glamor_finish_access_pixmap(pixmap);
+
     if (pixmap_priv->bo && pixmap_priv->owned_bo)
         gbm_bo_destroy(pixmap_priv->bo);
     
@@ -489,9 +491,11 @@ glamor_make_pixmap_exportable(PixmapPtr pixmap, Bool modifiers_ok)
 
     scratch_gc = GetScratchGC(pixmap->drawable.depth, screen);
     ValidateGC(&pixmap->drawable, scratch_gc);
+    pixmap_priv->exporting = TRUE;
     (void) scratch_gc->ops->CopyArea(&pixmap->drawable, &exported->drawable,
                               scratch_gc,
                               0, 0, width, height, 0, 0);
+    pixmap_priv->exporting = FALSE;
     FreeScratchGC(scratch_gc);
 
     /* Now, swap the tex/gbm/EGLImage/etc. of the exported pixmap into
@@ -906,8 +910,7 @@ glamor_egl_destroy_pixmap(PixmapPtr pixmap)
         struct glamor_pixmap_private *pixmap_priv =
             glamor_get_pixmap_private(pixmap);
 
-        if (pixmap_priv->bo)
-            gbm_bo_destroy(pixmap_priv->bo);
+        glamor_egl_set_pixmap_bo(pixmap, NULL, pixmap_priv->used_modifiers);
     }
 
     screen->DestroyPixmap = glamor_egl->saved_destroy_pixmap;
@@ -932,16 +935,15 @@ glamor_egl_exchange_buffers(PixmapPtr front, PixmapPtr back)
 
     glamor_pixmap_exchange_fbos(front, back);
 
-    glamor_finish_access(&front->drawable);
-    glamor_finish_access(&back->drawable);
+    glamor_finish_access_pixmap(front);
+    glamor_finish_access_pixmap(back);
 
     /* Swap all buffer related members */
     GLAMOR_EXCHANGE(back_priv->bo, front_priv->bo);
     GLAMOR_EXCHANGE(back_priv->owned_bo, front_priv->owned_bo);
     GLAMOR_EXCHANGE(back_priv->used_modifiers, front_priv->used_modifiers);
-//  GLAMOR_EXCHANGE(back_priv->bo_mapped, front_priv->bo_mapped);
-//  GLAMOR_EXCHANGE(back_priv->map_data, front_priv->map_data);
-//  GLAMOR_EXCHANGE(back_priv->gl_synced, front_priv->gl_synced);
+    GLAMOR_EXCHANGE(back_priv->bo_mapped, front_priv->bo_mapped);
+    GLAMOR_EXCHANGE(back_priv->map_data, front_priv->map_data);
 
     GLAMOR_EXCHANGE(back->devPrivate.ptr, front->devPrivate.ptr);
     GLAMOR_EXCHANGE(back->devKind, front->devKind);
@@ -963,8 +965,7 @@ glamor_egl_close_screen(ScreenPtr screen)
     screen_pixmap = screen->GetScreenPixmap(screen);
     pixmap_priv = glamor_get_pixmap_private(screen_pixmap);
 
-    gbm_bo_destroy(pixmap_priv->bo);
-    pixmap_priv->bo = NULL;
+    glamor_egl_set_pixmap_bo(screen_pixmap, NULL, pixmap_priv->used_modifiers);
 
     if (glamor_egl->device_path) {
         free(glamor_egl->device_path);

glamor/glamor_prepare.c
@@ -64,7 +64,7 @@ glamor_prep_drawable_box(DrawablePtr drawable, glamor_access_t access, BoxPtr bo
          * by a lower level driver
          */
         if (!priv->prepared)
-            return TRUE;
+            goto done;
 
         /* In X, multiple Drawables can be stored in the same Pixmap (such as
          * each individual window in a non-composited screen pixmap, or the
@@ -76,7 +76,7 @@ glamor_prep_drawable_box(DrawablePtr drawable, glamor_access_t access, BoxPtr bo
          */
         RegionSubtract(&region, &region, &priv->prepare_region);
         if (!RegionNotEmpty(&region))
-            return TRUE;
+            goto done;
 
         if (access == GLAMOR_ACCESS_RW)
             FatalError("attempt to remap buffer as writable");
@@ -87,7 +87,25 @@ glamor_prep_drawable_box(DrawablePtr drawable, glamor_access_t access, BoxPtr bo
             pixmap->devPrivate.ptr = NULL;
         }
     } else {
+#ifdef GLAMOR_HAS_GBM_MAP
         RegionInit(&priv->prepare_region, box, 1);
+
+        if (!priv->exporting && priv->bo) {
+            const struct gbm_bo *gbm = priv->bo;
+            uint32_t stride;
+
+            if ((pixmap->devPrivate.ptr = gbm_bo_map(gbm, 0, 0, pixmap->drawable.width,
+                                                     pixmap->drawable.height,
+                                                     (access == GLAMOR_ACCESS_RW) ?
+                                                     GBM_BO_TRANSFER_READ_WRITE : GBM_BO_TRANSFER_READ,
+                                                     &stride, &priv->map_data))) {
+                pixmap->devKind = stride;
+                priv->bo_mapped = TRUE;
+                priv->map_access = access;
+                goto done;
+            }
+        }
+#endif
 
         if (!glamor_priv->is_gles) {
             if (priv->pbo == 0)
@@ -129,8 +147,6 @@ glamor_prep_drawable_box(DrawablePtr drawable, glamor_access_t access, BoxPtr bo
     glamor_download_boxes(drawable, RegionRects(&region), RegionNumRects(&region),
                           0, 0, 0, 0, pixmap->devPrivate.ptr, pixmap->devKind);
 
-    RegionUninit(&region);
-
     if (priv->pbo) {
         if (priv->map_access == GLAMOR_ACCESS_RW)
             gl_access = GL_READ_WRITE;
@@ -139,6 +155,17 @@ glamor_prep_drawable_box(DrawablePtr drawable, glamor_access_t access, BoxPtr bo
 
         pixmap->devPrivate.ptr = glMapBuffer(GL_PIXEL_PACK_BUFFER, gl_access);
         glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+    }
+
+done:
+    RegionUninit(&region);
+
+    if (priv->bo_mapped) {
+        /* Finish all commands before accessing the buffer */
+        glamor_finish(screen);
+
+        /* No prepared flag for directly mapping */
+        return TRUE;
     }
 
     priv->prepared = TRUE;
@@ -158,6 +185,17 @@ glamor_finish_access(DrawablePtr drawable)
 
     if (!GLAMOR_PIXMAP_PRIV_HAS_FBO(priv))
         return;
+
+#ifdef GLAMOR_HAS_GBM
+    if (priv->bo_mapped) {
+        if (priv->prepared)
+            FatalError("Buffer mapping failed");
+
+        gbm_bo_unmap(priv->bo, priv->map_data);
+        pixmap->devPrivate.ptr = NULL;
+        priv->bo_mapped = FALSE;
+    }
+#endif
 
     if (!priv->prepared)
         return;
@@ -300,4 +338,10 @@ glamor_finish_access_gc(GCPtr gc)
         glamor_finish_access(&gc->stipple->drawable);
         break;
     }
+}
+
+void
+glamor_finish_access_pixmap(PixmapPtr pixmap)
+{
+    glamor_finish_access(&pixmap->drawable);
 }

glamor/glamor_prepare.h
@@ -49,4 +49,7 @@ glamor_prepare_access_gc(GCPtr gc);
 void
 glamor_finish_access_gc(GCPtr gc);
 
+void
+glamor_finish_access_pixmap(PixmapPtr pixmap);
+
 #endif /* _GLAMOR_PREPARE_H_ */

glamor/glamor_priv.h
@@ -401,6 +401,9 @@ typedef struct glamor_pixmap_private {
     struct gbm_bo *bo;
     Bool owned_bo;
     Bool used_modifiers;
+    Bool bo_mapped;
+    Bool exporting;
+    void *map_data;
 #endif
     /** block width of this large pixmap. */
     int block_w;

include/meson.build
@@ -116,6 +116,8 @@ conf_data.set('GBM_BO_FD_FOR_PLANE',
               build_glamor and gbm_dep.found() and gbm_dep.version().version_compare('>= 21.1') ? '1' : false)
 conf_data.set('GBM_BO_WITH_MODIFIERS2',
               build_glamor and gbm_dep.found() and gbm_dep.version().version_compare('>= 21.3') ? '1' : false)
+conf_data.set('GLAMOR_HAS_GBM_MAP', 
+              build_glamor and gbm_dep.found() and gbm_dep.version().version_compare('>= 17.4') ? '1' : false)
 conf_data.set('HAVE_EGL_SUPPORT', epoxy_dep.found() and gbm_dep.found())
 
 conf_data.set_quoted('SERVER_MISC_CONFIG_PATH', serverconfigdir)

commit 13a44c1d5669f137a7f222216a54fd616c0652e2

glamor: gbm_bo_map fixes
Fixes: aa1af2513a3bc508815f6a4249728662ddf1b49d ("glamor: Prefer using gbm_bo_map/gbm_bo_unmap.")
Fixes: a585ce2262ebf64c5382fe15385b65864ebe1e81 ("glamor,modesetting: Store gbm_bo in glamor_pixmap_private instead of egl image")

glamor/glamor_egl.c
@@ -511,7 +511,7 @@ glamor_make_pixmap_exportable(PixmapPtr pixmap, Bool modifiers_ok)
     return TRUE;
 }
 
-static struct gbm_bo *
+static inline struct gbm_bo *
 glamor_gbm_bo_from_pixmap_internal(ScreenPtr screen, PixmapPtr pixmap)
 {
     struct glamor_pixmap_private *pixmap_priv =
@@ -618,7 +618,6 @@ glamor_egl_fd_from_pixmap(ScreenPtr screen, PixmapPtr pixmap,
     fd = gbm_bo_get_fd(bo);
     *stride = gbm_bo_get_stride(bo);
     *size = *stride * gbm_bo_get_height(bo);
-    gbm_bo_destroy(bo);
 
     return fd;
 #else

glamor/glamor_prepare.c
@@ -342,7 +342,7 @@ glamor_finish_access_gc(GCPtr gc)
     }
 }
 
-void
+inline void
 glamor_finish_access_pixmap(PixmapPtr pixmap)
 {
     glamor_finish_access(&pixmap->drawable);

hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -666,6 +666,7 @@ drmmode_bo_destroy(drmmode_ptr drmmode, drmmode_bo *bo)
 #ifdef GLAMOR_HAS_GBM_MAP
         if (bo->gbm_ptr) {
             gbm_bo_unmap(bo->gbm, bo->gbm_map_data);
+            bo->gbm_map_data = NULL;
             bo->gbm_ptr = NULL;
         }
 #endif
@@ -3285,6 +3286,14 @@ drmmode_set_pixmap_bo(drmmode_ptr drmmode, PixmapPtr pixmap, drmmode_bo *bo)
     if (!drmmode->glamor)
         return TRUE;
 
+    /* Calling egl_create_textured_pixmap_from_gbm_bo in GLAMOR will
+     * destroy the GBM BO, we need to unmap pointers ahead of time. */
+    if (bo->gbm && bo->gbm_ptr) {
+        gbm_bo_unmap(bo->gbm, bo->gbm_map_data);
+        bo->gbm_ptr = NULL;
+        bo->gbm_map_data = NULL;
+    }
+
     if (!ms->glamor.egl_create_textured_pixmap_from_gbm_bo(pixmap, bo->gbm,
                                                            bo->used_modifiers)) {
         xf86DrvMsg(scrn->scrnIndex, X_ERROR, "Failed to create pixmap\n");
@@ -3303,10 +3312,7 @@ drmmode_glamor_handle_new_screen_pixmap(drmmode_ptr drmmode)
     ScreenPtr screen = xf86ScrnToScreen(drmmode->scrn);
     PixmapPtr screen_pixmap = screen->GetScreenPixmap(screen);
 
-    if (!drmmode_set_pixmap_bo(drmmode, screen_pixmap, &drmmode->front_bo))
-        return FALSE;
-
-    return TRUE;
+    return drmmode_set_pixmap_bo(drmmode, screen_pixmap, &drmmode->front_bo);
 }
 
 static Bool