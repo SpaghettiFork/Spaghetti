notbabaisyou/SpaghettiDev

commit d2ecdbac8c6aebbd0aa5e7d8473f724584174e65

modesetting: Re-enable more optimal modifiers

hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -802,9 +802,24 @@ drmmode_create_bo(drmmode_ptr drmmode, drmmode_bo *bo,
             break;
         }
 
+#ifdef GBM_BO_WITH_MODIFIERS2
+        uint64_t *modifiers;
+        uint32_t num_modifiers =
+                get_modifiers_set(drmmode->scrn, format, &modifiers, FALSE, TRUE, TRUE);
+
+        bo->gbm = gbm_bo_create_with_modifiers2(drmmode->gbm, 
+                                                width, height, format,
+                                                modifiers, num_modifiers,
+                                                GBM_BO_USE_RENDERING | GBM_BO_USE_SCANOUT);
+        bo->used_modifiers = (bo->gbm && num_modifiers);
+        
+        if (modifiers)
+            free(modifiers);
+#else
         bo->gbm = gbm_bo_create(drmmode->gbm, width, height, format,
                                 GBM_BO_USE_RENDERING | GBM_BO_USE_SCANOUT);
         bo->used_modifiers = FALSE;
+#endif
         return bo->gbm != NULL;
     }
 #endif
@@ -1982,8 +1997,13 @@ populate_format_modifiers(xf86CrtcPtr crtc, const drmModePlane *kplane,
 
             if ((i < mod->offset) || (i > mod->offset + 63))
                 continue;
+
             if (!(mod->formats & (1 << (i - mod->offset))))
                 continue;
+            
+            /* We should never arrive on this. */
+            if (mod->modifier == DRM_FORMAT_MOD_INVALID)
+                break;
 
             num_modifiers++;
             tmp = realloc(modifiers, num_modifiers * sizeof(modifiers[0]));

SpaghettiFork/Spaghetti

commit bfb7f16bc8a1e5d971df4729416753552d24f33e

modesetting: Two small nits
1. Move the the missing modifier/INVALID check into a seperate function.
2. We should continue trying to fetch modifiers despite INVALID

Fixes: d2ecdbac8c6aebbd0aa5e7d8473f724584174e65

hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -153,6 +153,12 @@ static drmmode_format_ptr get_format(drmmode_crtc_private_ptr drmmode_crtc,
         return &drmmode_crtc->formats[i];
 }
 
+static inline Bool
+drmmode_is_implicit_modifiers(drmmode_format_ptr iter, uint64_t modifier)
+{
+    return !iter->num_modifiers || modifier == DRM_FORMAT_MOD_INVALID;
+}
+
 Bool
 drmmode_is_format_supported(ScrnInfoPtr scrn, uint32_t format,
                             uint64_t modifier, Bool async_flip)
@@ -180,8 +186,7 @@ drmmode_is_format_supported(ScrnInfoPtr scrn, uint32_t format,
             if (iter->format != format)
                 continue;
 
-            if (modifier == DRM_FORMAT_MOD_INVALID ||
-                iter->num_modifiers == 0) {
+            if (drmmode_is_implicit_modifiers(iter, modifier)) {
                 found = TRUE;
                 break;
             }
@@ -2174,7 +2179,7 @@ populate_format_modifiers(xf86CrtcPtr crtc, const drmModePlane *kplane,
             
             /* We should never arrive on this. */
             if (mod->modifier == DRM_FORMAT_MOD_INVALID)
-                break;
+                continue;
 
             num_modifiers++;
             tmp = realloc(modifiers, num_modifiers * sizeof(modifiers[0]));