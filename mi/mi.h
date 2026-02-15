731351066a3d16fd4276664782ad8f2a2ea9bdf9
kohnish <kohnish@gmx.com>
2026-02-15 13:04:57 +0100

    Revert "mi: unexport miMarkOverlappedWindows()"
    This reverts commit ade6650a.

--- mi/mi.h
+++ mi/mi.h
@@ -286,6 +286,10 @@ extern _X_EXPORT void miPolylines(DrawablePtr pDrawable,
 
 /* miwindow.c */
 
+extern _X_EXPORT Bool miMarkOverlappedWindows(WindowPtr   /*pWin */,
+                                              WindowPtr   /*pFirst */,
+                                              WindowPtr * /*ppLayerWin */);
+
 extern _X_EXPORT void miHandleValidateExposures(WindowPtr /*pWin */);
 
 extern _X_EXPORT void miMoveWindow(WindowPtr    /*pWin */,
