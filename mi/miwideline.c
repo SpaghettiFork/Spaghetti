8e28203db48955e7c28c3bf41c5c2d91cefa7e4f
kohnish <kohnish@gmx.com>
2026-02-15 13:35:07 +0100

    Revert "mi: unexport miStepDash()"
    This reverts commit 8a6bbc80.

--- mi/miwideline.c
+++ mi/miwideline.c
@@ -64,12 +64,11 @@ SOFTWARE.
 #endif
 #include <X11/X.h>
 
-#include "mi/mi_priv.h"
-
 #include "windowstr.h"
 #include "gcstruct.h"
 #include "regionstr.h"
 #include "miwideline.h"
+#include "mi.h"
 
 typedef struct {
     int count;                  /* number of spans                  */
