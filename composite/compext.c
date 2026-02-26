notbabaisyou/SpaghettiDev

commit e34f6ca44f241c0cd407947e5a0daf0008b7d6cd

composite: Fix some possible memory leaks

composite/compext.c
@@ -734,14 +734,18 @@ PanoramiXCompositeNameWindowPixmap(ClientPtr client)
             return BadMatch;
         }
 
-        if (!AddResource(newPix->info[i].id, X11_RESTYPE_PIXMAP, (void *) pPixmap))
+        if (!AddResource(newPix->info[i].id, X11_RESTYPE_PIXMAP, (void *) pPixmap)) {
+            free(newPix);
             return BadAlloc;
+        }
 
         ++pPixmap->refcnt;
     }
 
-    if (!AddResource(stuff->pixmap, XRT_PIXMAP, (void *) newPix))
+    if (!AddResource(stuff->pixmap, XRT_PIXMAP, (void *) newPix)) {
+        free(newPix);
         return BadAlloc;
+    }
 
     return Success;
 }

SpaghettiFork/Spaghetti

commit 125b0a955fb3a73d4cc16f4a726ce6a082bce118

composite: Fix double-free in PanoramiXCompositeNameWindowPixmap.
Only needed the free() in one extra spot.

Fixes: e34f6ca44f241c0cd407947e5a0daf0008b7d6cd
Backport-to: 26.0.x

composite/compext.c
@@ -743,7 +743,6 @@ PanoramiXCompositeNameWindowPixmap(ClientPtr client)
     }
 
     if (!AddResource(stuff->pixmap, XRT_PIXMAP, (void *) newPix)) {
-        free(newPix);
         return BadAlloc;
     }
 