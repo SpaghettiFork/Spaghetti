notbabaisyou/SpaghettiDev

commit ed90162c086ccfcc94603b7e511a3c22779c9cae

dri3: Free formats on CloseScreen.
Direct leak of 1040 byte(s) in 1 object(s) allocated from:
    0x769755818bf0 in calloc /usr/src/debug/gcc/gcc/libsanitizer/lsan/lsan_interceptors.cpp:91
    0x635b83ec11c4 in cache_formats_and_modifiers ../spaghetti/dri3/dri3_screen.c:186
    0x635b83ec11c4 in dri3_get_supported_modifiers ../spaghetti/dri3/dri3_screen.c:233
    0x635b83ec0512 in proc_dri3_get_supported_modifiers ../spaghetti/dri3/dri3_request.c:395

dri3/dri3.c
@@ -36,6 +36,9 @@ dri3_close_screen(ScreenPtr screen)
 
     unwrap(screen_priv, screen, CloseScreen);
 
+    if (screen_priv->formats)
+        free(screen_priv->formats);
+
     free(screen_priv);
     return (*screen->CloseScreen) (screen);
 }

SpaghettiFork/Spaghetti

commit 86bff9e7015fd5596113e5f67c1eaeb0852852d3

dri3: Also free the modifiers array
Fixes: ed90162c086ccfcc94603b7e511a3c22779c9cae ("dri3: Free formats on CloseScreen.")

dri3/dri3.c
@@ -29,6 +29,15 @@ DevPrivateKeyRec dri3_screen_private_key;
 
 static int dri3_screen_generation;
 
+static inline void
+dri3_free_formats(dri3_screen_priv_ptr screen_priv)
+{
+    for (int i = 0; i < screen_priv->num_formats; i++)
+        free(screen_priv->formats[i].modifiers);
+
+    free(screen_priv->formats);
+}
+
 static Bool
 dri3_close_screen(ScreenPtr screen)
 {
@@ -36,8 +45,7 @@ dri3_close_screen(ScreenPtr screen)
 
     unwrap(screen_priv, screen, CloseScreen);
 
-    if (screen_priv->formats)
-        free(screen_priv->formats);
+    dri3_free_formats(screen_priv);
 
     free(screen_priv);
     return (*screen->CloseScreen) (screen);