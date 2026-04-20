notbabaisyou/SpaghettiDev

Fix drmModeCreatePropertyBlob() length parameter after f894801f

hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -1931,7 +1931,8 @@ drmmode_set_gamma_lut(drmmode_crtc_private_ptr drmmode_crtc,
     drmmode_prop_info_ptr gamma_lut_info =
         &drmmode_crtc->props[DRMMODE_CRTC_GAMMA_LUT];
     const uint32_t crtc_id = drmmode_crtc->mode_crtc->crtc_id;
-    struct drm_color_lut *lut = calloc(size, sizeof(struct drm_color_lut));
+    size_t lut_sz = sizeof(struct drm_color_slut) * size;
+    struct drm_color_lut *lut = calloc(1, lut_sz);
     if (!lut)
         return;
 
@@ -1945,7 +1946,7 @@ drmmode_set_gamma_lut(drmmode_crtc_private_ptr drmmode_crtc,
     }
 
     uint32_t blob_id;
-    if (drmModeCreatePropertyBlob(drmmode->fd, lut, sizeof(lut), &blob_id)) {
+    if (drmModeCreatePropertyBlob(drmmode->fd, lut, lut_sz, &blob_id)) {
         free(lut);
         return;
     }

SpaghettiFork/Spaghetti

hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -1906,7 +1906,8 @@ drmmode_set_gamma_lut(drmmode_crtc_private_ptr drmmode_crtc,
     drmmode_prop_info_ptr gamma_lut_info =
         &drmmode_crtc->props[DRMMODE_CRTC_GAMMA_LUT];
     const uint32_t crtc_id = drmmode_crtc->mode_crtc->crtc_id;
-    struct drm_color_lut *lut = calloc(size, sizeof(struct drm_color_lut));
+    size_t lut_sz = sizeof(struct drm_color_lut) * size;
+    struct drm_color_lut *lut = calloc(1, lut_sz);
     if (!lut)
         return;
 
@@ -1920,9 +1921,7 @@ drmmode_set_gamma_lut(drmmode_crtc_private_ptr drmmode_crtc,
     }
 
     uint32_t blob_id;
-    if (drmModeCreatePropertyBlob(drmmode->fd, lut,
-                                  sizeof(struct drm_color_lut) * size,
-                                  &blob_id)) {
+    if (drmModeCreatePropertyBlob(drmmode->fd, lut, lut_sz, &blob_id)) {
         free(lut);
         return;
     }