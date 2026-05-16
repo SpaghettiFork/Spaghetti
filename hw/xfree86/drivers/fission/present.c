f2efbfe8caa025179aa5a9622ccd2a44e7b025a4
notbabaisyou <though-went-some-simple@proton.me>
2026-05-16 16:42:44 +0200

    modesetting,fission: Guard rotate_bo.gbm behind an ifdef

--- hw/xfree86/drivers/fission/present.c
+++ hw/xfree86/drivers/fission/present.c
@@ -254,8 +254,10 @@ ms_present_check_unflip(RRCrtcPtr crtc,
         drmmode_crtc_private_ptr drmmode_crtc = config->crtc[i]->driver_private;
 
         /* Don't do pageflipping if CRTCs are rotated. */
+#ifdef GLAMOR_HAS_GBM
         if (drmmode_crtc->rotate_bo.gbm)
             return FALSE;
+#endif
 
         if (xf86_crtc_on(config->crtc[i]))
             num_crtcs_on++;
