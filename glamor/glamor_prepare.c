SpaghettiFork/Spaghetti

commit 86ef74772f28b1c2d62cf90af7e69e24f5cac6ee

glamor: Minor tweaks for GLES

glamor/glamor_prepare.c
@@ -30,6 +30,20 @@
  * and downloading all of the FBOs into it.
  */
 
+static inline int
+glamor_flags_for_map(glamor_screen_private *glamor_priv, glamor_access_t map_access)
+{
+#define GL_MAP_READ_WRITE (GL_MAP_READ_BIT | GL_MAP_WRITE_BIT)
+    Bool is_rw = map_access == GLAMOR_ACCESS_RW;
+
+    if (glamor_priv->has_map_buffer_range) {
+        return is_rw ? GL_MAP_READ_WRITE : GL_MAP_READ_BIT;
+    } else {
+        return is_rw ? GL_READ_WRITE : GL_READ_ONLY;
+    }
+#undef GL_MAP_READ_WRITE
+}
+
 static Bool
 glamor_prep_drawable_box(DrawablePtr drawable, glamor_access_t access, BoxPtr box)
 {
@@ -132,12 +146,17 @@ glamor_prep_drawable_box(DrawablePtr drawable, glamor_access_t access, BoxPtr bo
     RegionUninit(&region);
 
     if (priv->pbo) {
-        if (priv->map_access == GLAMOR_ACCESS_RW)
-            gl_access = GL_READ_WRITE;
-        else
-            gl_access = GL_READ_ONLY;
+        gl_access = glamor_flags_for_map(glamor_priv, priv->map_access);
 
-        pixmap->devPrivate.ptr = glMapBuffer(GL_PIXEL_PACK_BUFFER, gl_access);
+        if (glamor_priv->has_map_buffer_range) {
+            pixmap->devPrivate.ptr = glMapBufferRange(GL_PIXEL_PACK_BUFFER, 0,
+                                                      pixmap->devKind *
+                                                      pixmap->drawable.height,
+                                                      gl_access);
+        } else {
+            pixmap->devPrivate.ptr = glMapBuffer(GL_PIXEL_PACK_BUFFER, gl_access);
+        }
+
         glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
     }
 

glamor/glamor.c
@@ -740,7 +740,8 @@ glamor_init(ScreenPtr screen, unsigned int flags)
     glamor_priv->has_khr_debug = epoxy_has_gl_extension("GL_KHR_debug");
     glamor_priv->has_map_buffer_range =
         epoxy_has_gl_extension("GL_ARB_map_buffer_range") ||
-        epoxy_has_gl_extension("GL_EXT_map_buffer_range");
+        epoxy_has_gl_extension("GL_EXT_map_buffer_range") ||
+        (glamor_priv->is_gles && gl_version >= 30);
     glamor_priv->has_buffer_storage =
         epoxy_has_gl_extension("GL_ARB_buffer_storage") ||
         epoxy_has_gl_extension("GL_EXT_buffer_storage");

glamor/glamor_priv.h
@@ -48,6 +48,7 @@
 
 #define GLAMOR_DEFAULT_PRECISION  \
     "#ifdef GL_ES\n"              \
+    "precision mediump sampler2D;\n" \
     "precision mediump float;\n"  \
     "#endif\n"
 

commit c803119942b1bfcb405553af0f8d0e53827dd08b

glamor: Allow PBOs on GLES in glamor_prep_drawable_box
Requires has_map_buffer_range.

Fixes: 86ef74772f28b1c2d62cf90af7e69e24f5cac6ee ("glamor: Minor tweaks for GLES")

glamor/glamor_prepare.c
@@ -103,7 +103,7 @@ glamor_prep_drawable_box(DrawablePtr drawable, glamor_access_t access, BoxPtr bo
     } else {
         RegionInit(&priv->prepare_region, box, 1);
 
-        if (!glamor_priv->is_gles) {
+        if (!glamor_priv->is_gles || glamor_priv->has_map_buffer_range) {
             if (priv->pbo == 0)
                 glGenBuffers(1, &priv->pbo);
 