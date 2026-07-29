SpaghettiFork/Spaghetti

commit c3fcb8227506004adebc452f0bd01a357408e1b9

glamor/dash: Add stippled double dashed line support
The existing double dashed line shader only handled FillSolid,
bailing out for FillStippled/FillOpaqueStippled and falling back
to software rendering. Add a new shader facet that combines the
dash pattern texture lookup with the stipple texture lookup,
discarding fragments where the stipple is transparent.

glamor/glamor_dash.c
@@ -43,7 +43,6 @@ static const char on_off_fs_exec[] =
     "       if (pattern == 0.0)\n"
     "               discard;\n";
 
-/* XXX deal with stippled double dashed lines once we have stippling support */
 static const char double_fs_exec[] =
     "       float pattern = texture(dash, vec2(dash_offset, 0.5)).w;\n"
     "       if (pattern == 0.0)\n"
@@ -51,6 +50,53 @@ static const char double_fs_exec[] =
     "       else\n"
     "               frag_color = fg;\n";
 
+static const char stippled_dash_vs_vars[] =
+    "in vec3 primitive;\n"
+    "out float dash_offset;\n";
+
+static const char stippled_dash_vs_exec[] =
+    "       dash_offset = primitive.z / dash_length;\n"
+    "       fill_pos = (fill_offset + primitive.xy) * fill_size_inv;\n"
+    GLAMOR_POS(gl_Position, primitive.xy);
+
+static const char stippled_dash_fs_vars[] =
+    "in float dash_offset;\n";
+
+static const char stippled_double_fs_exec[] =
+    "       float pattern = texture(dash, vec2(dash_offset, 0.5)).w;\n"
+    "       float a = texture(sampler, fill_pos).w;\n"
+    "       if (a == 0.0)\n"
+    "               discard;\n"
+    "       if (pattern == 0.0)\n"
+    "               frag_color = bg;\n"
+    "       else\n"
+    "               frag_color = fg;\n";
+
+static Bool
+use_stippled_double_dash(DrawablePtr drawable, GCPtr gc, glamor_program *prog, void *arg)
+{
+    if (!glamor_set_stippled(drawable, gc, prog->fg_uniform,
+                             prog->fill_offset_uniform,
+                             prog->fill_size_inv_uniform))
+        return FALSE;
+    glamor_set_color(drawable, gc->bgPixel, prog->bg_uniform);
+    return TRUE;
+}
+
+static const glamor_facet glamor_facet_stippled_double_dash_lines = {
+    .version = 130,
+    .name = "poly_lines_stippled_double_dash",
+    .vs_vars = stippled_dash_vs_vars,
+    .vs_exec = stippled_dash_vs_exec,
+    .fs_vars = stippled_dash_fs_vars,
+    .fs_exec = stippled_double_fs_exec,
+    .locations = (glamor_program_location_dash |
+                  glamor_program_location_fg |
+                  glamor_program_location_bg |
+                  glamor_program_location_fillsamp |
+                  glamor_program_location_fillpos),
+    .use = use_stippled_double_dash,
+};
 
 static const glamor_facet glamor_facet_on_off_dash_lines = {
     .version = 130,
@@ -163,23 +209,40 @@ glamor_dash_setup(DrawablePtr drawable, GCPtr gc)
             goto bail;
         break;
     case LineDoubleDash:
-        if (gc->fillStyle != FillSolid)
-            goto bail;
+        switch (gc->fillStyle) {
+        case FillSolid:
+            prog = &glamor_priv->double_dash_line_prog;
 
-        prog = &glamor_priv->double_dash_line_prog;
+            if (!prog->prog) {
+                if (!glamor_build_program(screen, prog,
+                                          &glamor_facet_double_dash_lines,
+                                          NULL, NULL, NULL))
+                    goto bail;
+            }
 
-        if (!prog->prog) {
-            if (!glamor_build_program(screen, prog,
-                                      &glamor_facet_double_dash_lines,
-                                      NULL, NULL, NULL))
+            if (!glamor_use_program(drawable, gc, prog, NULL))
                 goto bail;
-        }
 
-        if (!glamor_use_program(drawable, gc, prog, NULL))
+            glamor_set_color(drawable, gc->fgPixel, prog->fg_uniform);
+            glamor_set_color(drawable, gc->bgPixel, prog->bg_uniform);
+            break;
+        case FillStippled:
+        case FillOpaqueStippled:
+            prog = &glamor_priv->stippled_double_dash_line_prog;
+
+            if (!prog->prog) {
+                if (!glamor_build_program(screen, prog,
+                                          &glamor_facet_stippled_double_dash_lines,
+                                          NULL, NULL, NULL))
+                    goto bail;
+            }
+
+            if (!glamor_use_program(drawable, gc, prog, NULL))
+                goto bail;
+            break;
+        default:
             goto bail;
-
-        glamor_set_color(drawable, gc->fgPixel, prog->fg_uniform);
-        glamor_set_color(drawable, gc->bgPixel, prog->bg_uniform);
+        }
         break;
 
     default:

glamor/glamor_priv.h
@@ -284,6 +284,7 @@ typedef struct glamor_screen_private {
     /*  glamor dash line shader */
     glamor_program_fill on_off_dash_line_progs;
     glamor_program      double_dash_line_prog;
+    glamor_program      stippled_double_dash_line_prog;
 
     /* glamor composite_glyphs shaders */
     glamor_program_render       glyphs_program;

commit 708abb9af2fb7e4ec72c78fc9c5f846eb9de2b1f

glamor/image: Fix FillOpaqueStippled
Fixes: c3fcb8227506004adebc452f0bd01a357408e1b9 ("glamor/dash: Add stippled double dashed line support")

glamor/glamor_dash.c
@@ -72,6 +72,14 @@ static const char stippled_double_fs_exec[] =
     "       else\n"
     "               frag_color = fg;\n";
 
+static const char stippled_opaque_double_fs_exec[] =
+    "       float pattern = texture(dash, vec2(dash_offset, 0.5)).w;\n"
+    "       float a = texture(sampler, fill_pos).w;\n"
+    "       if (a == 0.0 || pattern == 0.0)\n"
+    "               frag_color = bg;\n"
+    "       else\n"
+    "               frag_color = fg;\n";
+
 static Bool
 use_stippled_double_dash(DrawablePtr drawable, GCPtr gc, glamor_program *prog, void *arg)
 {
@@ -90,6 +98,21 @@ static const glamor_facet glamor_facet_stippled_double_dash_lines = {
     .vs_exec = stippled_dash_vs_exec,
     .fs_vars = stippled_dash_fs_vars,
     .fs_exec = stippled_double_fs_exec,
+    .locations = (glamor_program_location_dash |
+                  glamor_program_location_fg |
+                  glamor_program_location_bg |
+                  glamor_program_location_fillsamp |
+                  glamor_program_location_fillpos),
+    .use = use_stippled_double_dash,
+};
+
+static const glamor_facet glamor_facet_stippled_opaque_double_dash_lines = {
+    .version = 130,
+    .name = "poly_lines_stippled_opaque_double_dash",
+    .vs_vars = stippled_dash_vs_vars,
+    .vs_exec = stippled_dash_vs_exec,
+    .fs_vars = stippled_dash_fs_vars,
+    .fs_exec = stippled_opaque_double_fs_exec,
     .locations = (glamor_program_location_dash |
                   glamor_program_location_fg |
                   glamor_program_location_bg |
@@ -227,12 +250,24 @@ glamor_dash_setup(DrawablePtr drawable, GCPtr gc)
             glamor_set_color(drawable, gc->bgPixel, prog->bg_uniform);
             break;
         case FillStippled:
-        case FillOpaqueStippled:
             prog = &glamor_priv->stippled_double_dash_line_prog;
 
             if (!prog->prog) {
                 if (!glamor_build_program(screen, prog,
                                           &glamor_facet_stippled_double_dash_lines,
+                                          NULL, NULL, NULL))
+                    goto bail;
+            }
+
+            if (!glamor_use_program(drawable, gc, prog, NULL))
+                goto bail;
+            break;
+        case FillOpaqueStippled:
+            prog = &glamor_priv->stippled_opaque_double_dash_line_prog;
+
+            if (!prog->prog) {
+                if (!glamor_build_program(screen, prog,
+                                          &glamor_facet_stippled_opaque_double_dash_lines,
                                           NULL, NULL, NULL))
                     goto bail;
             }

glamor/glamor_priv.h
@@ -285,6 +285,7 @@ typedef struct glamor_screen_private {
     glamor_program_fill on_off_dash_line_progs;
     glamor_program      double_dash_line_prog;
     glamor_program      stippled_double_dash_line_prog;
+    glamor_program      stippled_opaque_double_dash_line_prog;
 
     /* glamor composite_glyphs shaders */
     glamor_program_render       glyphs_program;