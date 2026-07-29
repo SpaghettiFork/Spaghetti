SpaghettiFork/Spaghetti

commit 092c08ce68d0ebc1993d941825b5b0d1b5f064a6

Xext/sync: Be more explicit with types in GetTime

Xext/sync.c
@@ -2546,13 +2546,13 @@ static int64_t *pnext_time;
 
 static void GetTime(void)
 {
-    unsigned long millis = GetTimeInMillis();
-    unsigned long maxis = Now >> 32;
+    CARD32 millis = GetTimeInMillis();
+    CARD32 maxis  = (CARD32)(Now >> 32);
 
-    if (millis < (Now & 0xffffffff))
+    if (millis < (CARD32)(Now & 0xffffffff))
         maxis++;
 
-    Now = ((int64_t)maxis << 32) | millis;
+    Now = ((INT64)maxis << 32) | (INT64)millis;
 }
 
 /*

commit 9669ed26101a7672c682453015701813d4b3a9dc

Xext/sync: Use int64_t over INT64
Fixes: 092c08ce68d0ebc1993d941825b5b0d1b5f064a6 ("Xext/sync: Be more explicit with types in GetTime")

Xext/sync.c
@@ -2606,7 +2606,7 @@ static void GetTime(void)
     if (millis < (CARD32)(Now & 0xffffffff))
         maxis++;
 
-    Now = ((INT64)maxis << 32) | (INT64)millis;
+    Now = ((int64_t)maxis << 32) | (int64_t)millis;
 }
 
 /*