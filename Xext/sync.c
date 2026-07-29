SpaghettiFork/Spaghetti

commit 0e74d62f62045f7fba9cbc46d82ae3894d6f0fcf

Xext/sync: Do something better in SyncAlarmTriggerFired

Xext/sync.c
@@ -568,20 +568,32 @@ SyncAlarmTriggerFired(SyncTrigger * pTrigger)
             return;
 
         paCounter = (SyncCounter *) pTrigger->pSync;
+        oldvalue  = paTrigger->test_value;
 
         /* "The alarm is updated by repeatedly adding delta to the
          *  value of the trigger and re-initializing it until it
          *  becomes FALSE."
          */
-        oldvalue = paTrigger->test_value;
+        if (paTrigger->test_type == XSyncPositiveComparison ||
+            paTrigger->test_type == XSyncNegativeComparison) {
 
-        /* XXX really should do something smarter here */
+            int64_t diff = paCounter->value - paTrigger->test_value;
+            int64_t steps = diff / pAlarm->delta + 1;
+            int64_t advance;
 
-        do {
+            if (INT64_MUL_OVERFLOW(steps, pAlarm->delta, &advance))
+                overflow = TRUE;
+            else
+                overflow = checked_int64_add(&paTrigger->test_value,
+                                             paTrigger->test_value, advance);
+
+#ifdef DEBUG
+            BUG_WARN((*paTrigger->CheckTrigger)(paTrigger, paCounter->value));
+#endif
+        } else {
             overflow = checked_int64_add(&paTrigger->test_value,
                                          paTrigger->test_value, pAlarm->delta);
-        } while (!overflow &&
-                 (*paTrigger->CheckTrigger) (paTrigger, paCounter->value));
+        }
 
         new_test_value = paTrigger->test_value;
         paTrigger->test_value = oldvalue;

Xext/syncsrv.h
@@ -148,4 +148,28 @@ SyncDeleteTriggerFromSyncObject(SyncTrigger * pTrigger);
 int
 SyncAddTriggerToSyncObject(SyncTrigger * pTrigger);
 
+#if defined(__GNUC__) || defined(__clang__)
+#  define INT64_MUL_OVERFLOW(a, b, res) __builtin_mul_overflow((a), (b), (res))
+#else
+#include <stdint.h>
+static inline int
+checked_int64_multiply(int64_t a, int64_t b, int64_t *result)
+{
+    /* Overflow if signs match and magnitude exceeds INT64_MAX,
+     * or signs differ and magnitude exceeds INT64_MIN. */
+    if (a != 0 && b != 0) {
+        if ((a > 0) == (b > 0)) {
+            if (a > 0 && a > INT64_MAX / b) return 1;
+            if (a < 0 && a < INT64_MAX / b) return 1;
+        } else {
+            if (a > 0 && b < INT64_MIN / a) return 1;
+            if (a < 0 && a < INT64_MIN / b) return 1;
+        }
+    }
+    *result = a * b;
+    return 0;
+}
+#define INT64_MUL_OVERFLOW(a, b, res) checked_int64_multiply((a), (b), (res))
+#endif
+
 #endif                          /* _SYNCSRV_H_ */

commit 39e66aeb378c5346a45d32bf73d597c2627d6b30

Xext/sync: Avoid overflow/UB in comparison-alarm rearm math
Fixes: 0e74d62f62045f7fba9cbc46d82ae3894d6f0fcf ("Xext/sync: Do something better in SyncAlarmTriggerFired")

Xext/sync.c
@@ -577,18 +577,29 @@ SyncAlarmTriggerFired(SyncTrigger * pTrigger)
         if (paTrigger->test_type == XSyncPositiveComparison ||
             paTrigger->test_type == XSyncNegativeComparison) {
 
-            int64_t diff = paCounter->value - paTrigger->test_value;
-            int64_t steps = diff / pAlarm->delta + 1;
-            int64_t advance;
+            int64_t diff, steps, advance;
 
-            if (INT64_MUL_OVERFLOW(steps, pAlarm->delta, &advance))
+            if (checked_int64_subtract(&diff, paCounter->value,
+                                       paTrigger->test_value)) {
                 overflow = TRUE;
-            else
-                overflow = checked_int64_add(&paTrigger->test_value,
-                                             paTrigger->test_value, advance);
+            }
+            else if (pAlarm->delta == -1 && diff == INT64_MIN) {
+                /* INT64_MIN / -1 is not representable as int64_t */
+                overflow = TRUE;
+            }
+            else {
+                steps = diff / pAlarm->delta + 1;
+
+                if (INT64_MUL_OVERFLOW(steps, pAlarm->delta, &advance))
+                    overflow = TRUE;
+                else
+                    overflow = checked_int64_add(&paTrigger->test_value,
+                                                 paTrigger->test_value, advance);
+            }
 
 #ifdef DEBUG
-            BUG_WARN((*paTrigger->CheckTrigger)(paTrigger, paCounter->value));
+            BUG_WARN(!overflow &&
+                     (*paTrigger->CheckTrigger) (paTrigger, paCounter->value));
 #endif
         } else {
             overflow = checked_int64_add(&paTrigger->test_value,