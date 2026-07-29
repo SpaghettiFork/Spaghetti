notbabaisyou/SpaghettiDev

commit dac2833b562632bea19e207a8e66adf592127d89

glamor/image: Accelerate XY images and bitmaps
This is based upon old patches from FreeDesktop with the approach slightly
modified from the original.

Provides a decent performance uplift.

On a 11 year old Ultrabook running in single-channel:

 Before    After    Operation
--------  --------  ---------
  4340.0   69700.0  PutImage XY 10x10 square
   989.0    1620.0  PutImage XY 100x100 square
    52.9      67.2  PutImage XY 500x500 square
  4400.0  211000.0  PutImage XYBitmap 10x10 square
  2650.0   41000.0  PutImage XYBitmap 100x100 square
   238.0    6020.0  PutImage XYBitmap 500x500 square
  3840.0   79100.0  ShmPutImage XY 10x10 square
   980.0    1720.0  ShmPutImage XY 100x100 square
    59.2      70.5  ShmPutImage XY 500x500 square
  2690.0    2690.0  ShmPutImage XYBitmap 10x10 square
  2500.0    2510.0  ShmPutImage XYBitmap 100x100 square
  1040.0    1040.0  ShmPutImage XYBitmap 500x500 square

Co-authored-by: Keith Packard <keithp@keithp.com>
Co-authored-by: Adam Jackson <ajax@redhat.com>

glamor/glamor_image.c
@@ -23,14 +23,15 @@
 #include "glamor_priv.h"
 #include "glamor_transfer.h"
 #include "glamor_transform.h"
+#include "glamor_program.h"
 
 /*
- * PutImage. Only does ZPixmap right now as other formats are quite a bit harder
+ * ZPixmap PutImage
  */
 
 static Bool
-glamor_put_image_gl(DrawablePtr drawable, GCPtr gc, int depth, int x, int y,
-                    int w, int h, int leftPad, int format, char *bits)
+glamor_put_image_zpixmap_gl(DrawablePtr drawable, GCPtr gc, int depth, int x, int y,
+                            int w, int h, int leftPad, int format, char *bits)
 {
     ScreenPtr screen = drawable->pScreen;
     glamor_screen_private *glamor_priv = glamor_get_screen_private(screen);
@@ -50,12 +51,6 @@ glamor_put_image_gl(DrawablePtr drawable, GCPtr gc, int depth, int x, int y,
         goto bail;
 
     if (!glamor_pm_is_solid(gc->depth, gc->planemask))
-        goto bail;
-
-    if (format == XYPixmap && drawable->depth == 1 && leftPad == 0)
-        format = ZPixmap;
-
-    if (format != ZPixmap)
         goto bail;
 
     x += drawable->x;
@@ -84,6 +79,232 @@ bail:
     return FALSE;
 }
 
+/*
+ * XYBitmap PutImage
+ */
+
+static const char vs_vars_put_bitmap[] =
+    "in vec4 primitive;\n"
+    "in vec2 source;\n"
+    "out vec2 image_pos;\n";
+
+static const char vs_exec_put_bitmap[] =
+    "       vec2 pos = primitive.zw * vec2(gl_VertexID&1, (gl_VertexID&2)>>1);\n"
+    GLAMOR_POS(gl_Position, (primitive.xy + pos))
+    "       image_pos = source + pos;\n";
+
+static const char fs_vars_put_bitmap[] =
+    "in vec2 image_pos;\n"
+    "uniform sampler2D font;\n"
+    "uniform vec2 bitmap_size;\n";
+
+static Bool
+glamor_put_bitmap_use(DrawablePtr drawable, GCPtr gc, glamor_program *prog, void *arg)
+{
+    GLint loc;
+
+    if (!glamor_set_solid(drawable, gc, TRUE, prog->fg_uniform))
+        return FALSE;
+
+    glamor_set_color(drawable, gc->bgPixel, prog->bg_uniform);
+
+    loc = glGetUniformLocation(prog->prog, "font");
+    if (loc >= 0)
+        glUniform1i(loc, 1);
+
+    return TRUE;
+}
+
+static const char fs_exec_put_bitmap[] =
+    "       ivec2 itile_texture = ivec2(image_pos);\n"
+#if BITMAP_BIT_ORDER == MSBFirst
+    "       int x = 7 - (itile_texture.x & 7);\n"
+#else
+    "       int x = itile_texture.x & 7;\n"
+#endif
+    "       itile_texture.x >>= 3;\n"
+    "       float byte_val = texture(font, (vec2(itile_texture) + 0.5) / bitmap_size).r;\n"
+    "       int texel = int(floor(byte_val * 255.0 + 0.5));\n"
+    "       int bit = (texel >> x) & 1;\n"
+    "       if (bit == 0)\n"
+    "               frag_color = bg;\n"
+    "       else\n"
+    "               frag_color = fg;\n";
+
+static const glamor_facet glamor_facet_put_bitmap = {
+    .name = "put_bitmap",
+    .version = 130,
+    .vs_vars = vs_vars_put_bitmap,
+    .vs_exec = vs_exec_put_bitmap,
+    .fs_vars = fs_vars_put_bitmap,
+    .fs_exec = fs_exec_put_bitmap,
+    .locations = glamor_program_location_fg |
+                 glamor_program_location_bg,
+    .source_name = "source",
+    .use = glamor_put_bitmap_use,
+};
+
+static Bool
+glamor_put_image_xybitmap_gl(DrawablePtr drawable, GCPtr gc, int x, int y,
+                             int w, int h, int leftPad, char *bits)
+{
+    ScreenPtr screen = drawable->pScreen;
+    glamor_screen_private *glamor_priv = glamor_get_screen_private(screen);
+    PixmapPtr pixmap = glamor_get_drawable_pixmap(drawable);
+    glamor_pixmap_private *pixmap_priv = glamor_get_pixmap_private(pixmap);
+    uint32_t byte_stride = BitmapBytePad(w + leftPad);
+    GLuint texture_id;
+    glamor_program *prog;
+    char *vbo_offset;
+    GLshort *v;
+    int box_index;
+    int off_x, off_y;
+    Bool ret = FALSE;
+
+    if (!GLAMOR_PIXMAP_PRIV_HAS_FBO(pixmap_priv))
+        return FALSE;
+
+    glamor_make_current(glamor_priv);
+
+    prog = &glamor_priv->put_bitmap_prog;
+    if (prog->failed)
+        goto bail;
+
+    if (!prog->prog) {
+        if (!glamor_build_program(screen, prog, &glamor_facet_put_bitmap, NULL, NULL, NULL)) {
+            goto bail;
+        }
+    }
+
+    if (!glamor_use_program(&pixmap->drawable, gc, prog, NULL))
+        goto bail;
+
+    /* Upload bitmap as GL_R8 (unsigned normalized) */
+    glGenTextures(1, &texture_id);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glamor_priv->suppress_gl_out_of_memory_logging = true;
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, byte_stride, h,
+                                0, GL_RED, GL_UNSIGNED_BYTE, bits);
+    glamor_priv->suppress_gl_out_of_memory_logging = false;
+    if (glGetError() == GL_OUT_OF_MEMORY)
+        goto bail_tex;
+
+    /* Set bitmap_size uniform for the fragment shader */
+    GLint loc = glGetUniformLocation(prog->prog, "bitmap_size");
+    if (loc >= 0)
+        glUniform2f(loc, (float)byte_stride, (float)h);
+
+    /* Set up vertex buffers for the 4 quad vertices */
+    v = glamor_get_vbo_space(screen, (4 * 6 * sizeof(GLshort)), &vbo_offset);
+
+    glEnableVertexAttribArray(GLAMOR_VERTEX_POS);
+    glVertexAttribPointer(GLAMOR_VERTEX_POS, 4, GL_SHORT, GL_FALSE,
+                          6 * sizeof(GLshort), vbo_offset);
+
+    glEnableVertexAttribArray(GLAMOR_VERTEX_SOURCE);
+    glVertexAttribPointer(GLAMOR_VERTEX_SOURCE, 2, GL_SHORT, GL_FALSE,
+                          6 * sizeof(GLshort), vbo_offset + 4 * sizeof(GLshort));
+
+    for (int i = 0; i < 4; i++) {
+        v[i * 6 + 0] = x;
+        v[i * 6 + 1] = y;
+        v[i * 6 + 2] = w;
+        v[i * 6 + 3] = h;
+        v[i * 6 + 4] = leftPad;
+        v[i * 6 + 5] = 0;
+    }
+
+    glamor_put_vbo_space(screen);
+    glEnable(GL_SCISSOR_TEST);
+
+    glamor_pixmap_loop(pixmap_priv, box_index) {
+        BoxPtr box = RegionRects(gc->pCompositeClip);
+        int nbox = RegionNumRects(gc->pCompositeClip);
+
+        glamor_set_destination_drawable(drawable, box_index, TRUE, FALSE,
+                                        prog->matrix_uniform,
+                                        &off_x, &off_y);
+
+        while (nbox--) {
+            glScissor(box->x1 + off_x,
+                      box->y1 + off_y,
+                      box->x2 - box->x1,
+                      box->y2 - box->y1);
+            box++;
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        }
+    }
+
+    glDisable(GL_SCISSOR_TEST);
+    glDisableVertexAttribArray(GLAMOR_VERTEX_SOURCE);
+    glDisableVertexAttribArray(GLAMOR_VERTEX_POS);
+
+    ret = TRUE;
+bail_tex:
+    glDeleteTextures(1, &texture_id);
+bail:
+    return ret;
+}
+
+/*
+ * XYPixmap PutImage
+ *
+ * Creates a CPU-only pixmap, lets fbPutImage handle the format
+ * conversion, then GPU-accelerated CopyArea to the destination.
+ */
+
+static Bool
+glamor_put_image_xy_gl(DrawablePtr drawable, GCPtr gc, int depth, int x, int y,
+                       int w, int h, int leftPad, int format, char *bits)
+{
+    PixmapPtr pixmap = glamor_get_drawable_pixmap(drawable);
+    glamor_pixmap_private *pixmap_priv = glamor_get_pixmap_private(pixmap);
+    ScreenPtr   screen = drawable->pScreen;
+    PixmapPtr   temp_pixmap = NULL;
+    DrawablePtr temp_drawable;
+    GCPtr       temp_gc;
+    Bool        ret = FALSE;
+    ChangeGCVal gcv[3];
+
+    if (!GLAMOR_PIXMAP_PRIV_HAS_FBO(pixmap_priv))
+        return FALSE;
+
+    temp_pixmap = (*screen->CreatePixmap)(screen, w, h, drawable->depth,
+                                          GLAMOR_CREATE_PIXMAP_CPU);
+    if (!temp_pixmap)
+        goto bail;
+
+    temp_drawable = &temp_pixmap->drawable;
+    temp_gc = GetScratchGC(temp_drawable->depth, screen);
+    if (!temp_gc)
+        goto bail_pixmap;
+
+    /* copy mode for the first plane to clear all of the other bits */
+    gcv[0].val = GXcopy;
+    gcv[1].val = gc->fgPixel;
+    gcv[2].val = gc->bgPixel;
+
+    ChangeGC(NullClient, temp_gc, GCFunction | GCForeground | GCBackground, gcv);
+    ValidateGC(temp_drawable, temp_gc);
+
+    (*temp_gc->ops->PutImage)(temp_drawable, temp_gc, depth, 0, 0, w, h, leftPad, format, bits);
+    (*gc->ops->CopyArea)(&temp_pixmap->drawable, drawable, gc, 0, 0, w, h, x, y);
+
+    ret = TRUE;
+
+    FreeScratchGC(temp_gc);
+bail_pixmap:
+    (*screen->DestroyPixmap)(temp_pixmap);
+bail:
+    return ret;
+}
+
 static void
 glamor_put_image_bail(DrawablePtr drawable, GCPtr gc, int depth, int x, int y,
                       int w, int h, int leftPad, int format, char *bits)
@@ -97,8 +318,24 @@ void
 glamor_put_image(DrawablePtr drawable, GCPtr gc, int depth, int x, int y,
                  int w, int h, int leftPad, int format, char *bits)
 {
-    if (glamor_put_image_gl(drawable, gc, depth, x, y, w, h, leftPad, format, bits))
-        return;
+    switch (format) {
+    case ZPixmap:
+        if (glamor_put_image_zpixmap_gl(drawable, gc, depth, x, y, w, h, leftPad, format, bits))
+            return;
+        break;
+    case XYPixmap:
+        if (glamor_put_image_xy_gl(drawable, gc, depth, x, y, w, h, leftPad, format, bits))
+            return;
+        break;
+    case XYBitmap:
+        if (w * h >= 100 * 100) {
+            if (glamor_put_image_xybitmap_gl(drawable, gc, x, y, w, h, leftPad, bits))
+                return;
+        }
+        if (glamor_put_image_xy_gl(drawable, gc, depth, x, y, w, h, leftPad, format, bits))
+            return;
+        break;
+    }
     glamor_put_image_bail(drawable, gc, depth, x, y, w, h, leftPad, format, bits);
 }
 

Xext/shm.c
@@ -497,7 +497,7 @@ doShmPutImage(DrawablePtr dst, GCPtr pGC,
             return;
 
         pPixmap = (*dst->pScreen->CreatePixmap) (dst->pScreen, sw, sh, depth,
-                                                 CREATE_PIXMAP_USAGE_SCRATCH);
+                                                 CREATE_PIXMAP_USAGE_XY_TO_Z);
         if (!pPixmap) {
             FreeScratchGC(putGC);
             return;

glamor/glamor.c
@@ -212,6 +212,7 @@ glamor_create_pixmap(ScreenPtr screen, int w, int h, int depth,
         return NullPixmap;
 
     if ((usage == GLAMOR_CREATE_PIXMAP_CPU
+         || usage == CREATE_PIXMAP_USAGE_XY_TO_Z
          || (usage == CREATE_PIXMAP_USAGE_GLYPH_PICTURE &&
              w <= glamor_priv->glyph_max_dim &&
              h <= glamor_priv->glyph_max_dim)

glamor/glamor_priv.h
@@ -272,6 +272,9 @@ typedef struct glamor_screen_private {
     glamor_program      copy_area_prog;
     glamor_program      copy_plane_prog;
 
+    /* glamor image shaders */
+    glamor_program      put_bitmap_prog;
+
     /* glamor line shader */
     glamor_program_fill poly_line_program;
 

include/scrnintstr.h
@@ -185,6 +185,8 @@ typedef void (*ClipNotifyProcPtr) (WindowPtr /*pWindow */ ,
 #define CREATE_PIXMAP_USAGE_GLYPH_PICTURE               3
 /* pixmap will be shared */
 #define CREATE_PIXMAP_USAGE_SHARED                      4
+/* pixmap will be used to convert a XYPixmap to a ZPixmap */
+#define CREATE_PIXMAP_USAGE_XY_TO_Z                     5
 
 typedef PixmapPtr (*CreatePixmapProcPtr) (ScreenPtr /*pScreen */ ,
                                           int /*width */ ,

mi/midispcur.c
@@ -195,9 +195,10 @@ miDCRealize(ScreenPtr pScreen, CursorPtr pCursor)
         if (!pFormat)
             return FALSE;
 
+        /* the cursor picture retains a reference, so this pixmap is persistent */
         pPixmap = (*pScreen->CreatePixmap) (pScreen, pCursor->bits->width,
                                             pCursor->bits->height, 32,
-                                            CREATE_PIXMAP_USAGE_SCRATCH);
+                                            0);
         if (!pPixmap)
             return FALSE;
 

SpaghettiFork/Spaghetti

commit 58c603bee829936a309690dea62cb06041f8f7f0

glamor/image: Handle glamor_get_vbo_space failure.
Also reset the active texture back to GL_TEXTURE0

Fixes: dac2833b562632bea19e207a8e66adf592127d89 ("glamor/image: Accelerate XY images and bitmaps")

glamor/glamor_image.c
@@ -202,6 +202,8 @@ glamor_put_image_xybitmap_gl(DrawablePtr drawable, GCPtr gc, int x, int y,
 
     /* Set up vertex buffers for the 4 quad vertices */
     v = glamor_get_vbo_space(screen, (4 * 6 * sizeof(GLshort)), &vbo_offset);
+    if (_X_UNLIKELY(!v))
+        goto bail_tex;
 
     glEnableVertexAttribArray(GLAMOR_VERTEX_POS);
     glVertexAttribPointer(GLAMOR_VERTEX_POS, 4, GL_SHORT, GL_FALSE,
@@ -247,6 +249,7 @@ glamor_put_image_xybitmap_gl(DrawablePtr drawable, GCPtr gc, int x, int y,
 
     ret = TRUE;
 bail_tex:
+    glActiveTexture(GL_TEXTURE0);
     glDeleteTextures(1, &texture_id);
 bail:
     return ret;